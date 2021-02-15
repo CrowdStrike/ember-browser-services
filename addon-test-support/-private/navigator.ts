5a6aef874365ce97dc1301d05399a2afd6151019
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-02-14 21:32:22 -0500

    chore: remove demo interaction code, rework tests
    Navigator tests now demonstrate how to interact with the navigator
    depending on the stubbing scenario.

--- addon-test-support/-private/navigator.ts
+++ addon-test-support/-private/navigator.ts
@@ -1,23 +1,12 @@
-import RSVP from 'rsvp';
 import { proxyService } from 'ember-browser-services/utils/proxy-service';
 import { testableVersionOf } from './-helpers';
 
 export type TestNavigator = typeof fakeNavigator;
 
-type UserInteractions<Source> = Partial<Record<keyof Source, RSVP.Deferred<unknown>>>;
-
 export const fakeMediaDevices = testableVersionOf(
   navigator.mediaDevices,
   class TestMediaDevices {
-    interactions: UserInteractions<MediaDevices> = {};
-
-    getUserMedia(_options: Parameters<typeof navigator.mediaDevices.getUserMedia>) {
-      let deferred = RSVP.defer();
-
-      this.interactions.getUserMedia = deferred;
-
-      return deferred.promise;
-    }
+    // overrides deliberately left empty
   },
 );
 
--- tests/unit/services/browser/navigator-test.ts
+++ tests/unit/services/browser/navigator-test.ts
@@ -1,18 +1,50 @@
 import { module, test } from 'qunit';
-import { setupBrowserFakes } from 'ember-browser-services/test-support';
+import RSVP from 'rsvp';
 import { setupTest } from 'ember-qunit';
+
+import { setupBrowserFakes } from 'ember-browser-services/test-support';
+
+import type ApplicationInstance from '@ember/application/instance';
+import type { NavigatorService } from 'ember-browser-services/types';
+
+function getNavigatorService(owner: ApplicationInstance) {
+  return owner.lookup('service:browser/navigator') as NavigatorService;
+}
 
 module('Service | browser/navigator', function (hooks) {
   setupTest(hooks);
-  setupBrowserFakes(hooks, { navigator: true });
 
-  module('mediaDevices', function () {
-    module('getUserMedia', function () {
+  module('for config: true', function (hooks) {
+    setupBrowserFakes(hooks, { navigator: true });
+
+    test('APIs fallback to browser APIs', function (assert) {
+      let service = getNavigatorService(this.owner);
+
+      assert.equal(service.mediaDevices.getUserMedia, navigator.mediaDevices.getUserMedia);
+    });
+  });
+
+  module('for config: object override', function () {
+    module('mediaDevices.getUserMedia', function (hooks) {
+      let deferred: RSVP.Deferred<string>;
+
+      setupBrowserFakes(hooks, {
+        navigator: {
+          mediaDevices: {
+            getUserMedia() {
+              deferred = RSVP.defer();
+
+              return deferred.promise;
+            },
+          },
+        },
+      });
+
       test('can be manually resolved', async function (assert) {
-        let service = this.owner.lookup('service:browser/navigator');
+        let service = getNavigatorService(this.owner);
         let request = service.mediaDevices.getUserMedia();
 
-        service.mediaDevices.interactions.getUserMedia.resolve('my custom media stream');
+        deferred.resolve('my custom media stream');
 
         let stream = await request;
 
@@ -20,10 +52,10 @@ module('Service | browser/navigator', function (hooks) {
       });
 
       test('can be rejected', async function (assert) {
-        let service = this.owner.lookup('service:browser/navigator');
+        let service = getNavigatorService(this.owner);
         let request = service.mediaDevices.getUserMedia();
 
-        service.mediaDevices.interactions.getUserMedia.reject('no camera access!');
+        deferred.reject('no camera access!');
 
         await assert.rejects(request, 'no camera access');
       });
