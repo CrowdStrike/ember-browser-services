659f20f702873c942d6476301730107c71a566c4
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-13 23:50:23 -0500

    feat: utilize ember-window-mock for the underlying provider
    ...of the services. This is _instead of_ custom implementations that
    suited the needs of the maintainers of this package. ember-window-mock
    uses a recursive proxy that allows any api to be stubbed, regardless of
    its ability to be stubbed on the native object (ie: can't set a
    read-only property).
    
    fix: there is now URL validation on initial location.href values
    
    BREAKING CHANGE: invalid hrefs will throw an error

--- addon-test-support/index.ts
+++ addon-test-support/index.ts
@@ -1,30 +1,38 @@
-import { FakeDocumentService, TestDocument } from './-private/document';
-import { FakeLocalStorageService } from './-private/local-storage';
-import { FakeWindowService, TestWindow } from './-private/window';
-import { FakeNavigatorService, TestNavigator } from './-private/navigator';
-import { maybeMake } from './-private/-helpers';
+import window from 'ember-window-mock';
+import Service from '@ember/service';
+import { proxyService } from 'ember-browser-services/utils/proxy-service';
+import { setupWindowMock } from 'ember-window-mock/test-support';
 
-import type Service from '@ember/service';
+import { FakeLocalStorageService } from './-private/local-storage';
+
 import type { TestContext } from 'ember-test-helpers';
+import type { RecursivePartial } from 'ember-browser-services/types';
+import { patchWindow } from './window-mock-augments';
 
 type Fakes = {
-  window?: boolean | TestWindow | typeof Service;
+  window?: boolean | typeof Service | RecursivePartial<Window>;
   localStorage?: boolean;
-  document?: boolean | TestDocument | typeof Service;
-  navigator?: boolean | TestNavigator;
+  document?: boolean | typeof Service | RecursivePartial<Document>;
+  navigator?: boolean | RecursivePartial<Navigator>;
 };
 
 export function setupBrowserFakes(hooks: NestedHooks, options: Fakes): void {
+  setupWindowMock(hooks);
+
   hooks.beforeEach(function (this: TestContext) {
     if (options.window) {
-      this.owner.register('service:browser/window', maybeMake(options.window, FakeWindowService));
+      // default, can still be overwritten
+      // see: https://github.com/kaliber5/ember-window-mock/issues/175
+      let patched = patchWindow(window);
+      let service = maybeMake(options.window, patched);
+
+      this.owner.register('service:browser/window', service);
     }
 
     if (options.document) {
-      this.owner.register(
-        'service:browser/document',
-        maybeMake(options.document, FakeDocumentService),
-      );
+      let service = maybeMake(options.document, window.document);
+
+      this.owner.register('service:browser/document', service);
     }
 
     if (options.localStorage) {
@@ -32,10 +40,57 @@ export function setupBrowserFakes(hooks: NestedHooks, options: Fakes): void {
     }
 
     if (options.navigator) {
-      this.owner.register(
-        'service:browser/navigator',
-        maybeMake(options.navigator, FakeNavigatorService),
-      );
+      let service = maybeMake(options.navigator, window.navigator);
+
+      this.owner.register('service:browser/navigator', service);
     }
   });
+}
+
+// this usage of any is correct, because it literally could be *any*thing
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnknownObject = Record<string, any>;
+
+export function maybeMake<DefaultType extends UnknownObject, TestClass extends UnknownObject>(
+  maybeImplementation: true | typeof Service | TestClass | RecursivePartial<DefaultType>,
+  target: DefaultType,
+): DefaultType {
+  if (maybeImplementation === true) {
+    return proxyService(target);
+  }
+
+  if (maybeImplementation.prototype instanceof Service) {
+    return target;
+  }
+
+  if (typeof maybeImplementation === 'object') {
+    applyStub(target, maybeImplementation);
+
+    return proxyService(target);
+  }
+
+  return proxyService(target);
+}
+
+// we are already using ember-window-mock, so the proxy internal to that package will
+// "just handle" setting stuff on the window
+//
+// NOTE:
+//  - Location implementation is incomplete:
+//     https://github.com/kaliber5/ember-window-mock/blob/2b8fbf581fc65e7f5455cd291497a3fdc2efdaf5/addon-test-support/-private/mock/location.js#L23
+//     - does not allow setting "origin"
+function applyStub(root: any, partial?: any) {
+  if (!partial) return root;
+
+  for (let key of Object.keys(partial)) {
+    let value = partial[key];
+
+    if (Array.isArray(value)) {
+      root[key] = value;
+    } else if (typeof value === 'object') {
+      applyStub(root[key], value);
+    } else {
+      root[key] = value;
+    }
+  }
 }
