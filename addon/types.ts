2333ddb1a0c375653a73290d7265400603f3a251
Drew Neil <andrew.jr.neil@gmail.com>
2021-11-16 09:28:02 +0000

    Create session-service

--- addon/types.ts
+++ addon/types.ts
@@ -1,11 +1,13 @@
 import type { default as _DocumentService } from './services/browser/document';
 import type { default as _LocalStorageService } from './services/browser/local-storage';
+import type { default as _SessionStorageService } from './services/browser/session-storage';
 import type { default as _NavigatorService } from './services/browser/navigator';
 import type { default as _WindowService } from './services/browser/window';
 
 export type WindowService = typeof _WindowService;
 export type DocumentService = typeof _DocumentService;
 export type LocalStorageService = typeof _LocalStorageService;
+export type SessionStorageService = typeof _SessionStorageService;
 export type NavigatorService = typeof _NavigatorService;
 
 export interface Class<T> {
---
+++ app/services/browser/session-storage.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-browser-services/services/browser/session-storage';
